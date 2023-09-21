a867210e1f55e6ee9089c36ba7f6277e0f939ba2
bryangoodrich <goodrich.bryan@gmail.com>
2023-09-21 16:58:08 -0700

    Update capstone app week 2

---
+++ Capstone/littlelemon/src/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <>
+            <header>
+                Image here.
+            </header>
+        </>
+    );
+}
+
+export default Header;
---
+++ Capstone/littlelemon/src/Nav.js
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+        <>
+            <nav>
+                Nav here.
+            </nav>
+        </>
+    );
+}
+
+export default Nav;
